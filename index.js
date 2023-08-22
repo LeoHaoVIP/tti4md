1da470fe3d6824d4ff90d0122222affa8a150555
Wenhao Zhao <2867555086@qq.com>
2023-08-22 11:35:17 +0800

    适配 vercel 部署

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require('express')
 const https = require("https")
 const http = require("http")
 const app = express()
-const port = 4000
-const warningSVG = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="288" height="20" role="img" aria-label="WARNING: MESSAGE"><title>WARNING: MESSAGE</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="r"><rect width="288" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="65" height="20" fill="#555"/><rect x="65" width="223" height="20" fill="#dfb317"/><rect width="288" height="20" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="335" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="550">WARNING</text><text x="335" y="140" transform="scale(.1)" fill="#fff" textLength="550">WARNING</text><text aria-hidden="true" x="1755" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="2130">MESSAGE</text><text x="1755" y="140" transform="scale(.1)" fill="#fff" textLength="2130">MESSAGE</text></g></svg>'
-
+//中间件配置
+//适应Post请求
+app.use(express.json())
+//适应get请求
+app.use(express.urlencoded({extended: false}))
 /**
  * URL encode is needed.
  * Online tools: https://www.urlencoder.net/
@@ -16,14 +18,16 @@ app.get('/api', async (request, respond) => {
     const selector = params.selector
     const start = params.start ? params.start : 0
     const end = params.end
-    const label = params.label
     const color = params.color ? params.color : 'brightgreen'
-    let imgShieldUrl = 'https://img.shields.io/badge/undefined-' + color;
-    if (label) {
-        imgShieldUrl = imgShieldUrl + '&label=' + label;
-    }
-    respond.set('Content-Type', 'image/svg+xml;charset=utf-8')
     try {
+        //参数校验
+        if (!url) {
+            throw new Error('no url found')
+        }
+        if (!selector) {
+            throw new Error('no selector found')
+        }
+        let imgShieldUrl = 'https://img.shields.io/badge/undefined-' + color;
         let browser = await puppeteer.launch({
             headless: true,
             ignoreHTTPSErrors: true,
@@ -46,14 +50,16 @@ app.get('/api', async (request, respond) => {
             throw new Error('invalid character found');
         }
         imgShieldUrl = 'https://img.shields.io/badge/' + message + '-' + color;
-        if (label) {
-            imgShieldUrl = imgShieldUrl + '&label=' + label;
-        }
         download(imgShieldUrl, (data) => {
+            respond.set('Content-Type', 'image/svg+xml;charset=utf-8')
             respond.send(data);
         })
     } catch (e) {
-        respond.send(warningSVG.replaceAll('MESSAGE', e.message))
+        console.log(e)
+        respond.set('Content-Type', 'application/json')
+        respond.send({
+            msg: e.message
+        })
     }
 })
 
@@ -92,7 +98,7 @@ function download(url, callback) {
     })
 }
 
-const server = http.createServer(app)
-server.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`app listening on port ${port}`)
+// })
+module.exports = app;
